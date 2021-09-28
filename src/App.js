37a92cfc03318c348372619872ba1007ca4faad4
Tim Teillor <timteillor@gmail.com>
2021-09-28 08:58:01 +0200

    Learn setState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ClassCounter from './components/ClassCounter';
+import Counter from './components/counter';
 
 function App() {
   return (
     <div className="App">
-      
+      <ClassCounter/>
     </div>
   );
 }
